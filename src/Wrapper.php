c6b9fbea8b534195537b86b5d3cac9cc01aed645
Jon Cram <jon@webignition.net>
2019-01-30 19:42:43 +0000

    Pass uri, documentCharacterSet to Wrapper::validate()

--- src/Wrapper.php
+++ src/Wrapper.php
@@ -6,25 +6,6 @@ use webignition\HtmlValidator\Output\Output;
 
 class Wrapper
 {
-    const DEFAULT_VALIDATOR_PATH = '/usr/local/validator/cgi-bin/check';
-    const CONFIG_KEY_DOCUMENT_URI = 'document-uri';
-    const CONFIG_KEY_DOCUMENT_CHARACTER_SET = 'document-character-set';
-    const CONFIG_KEY_PARSER_CONFIGURATION_VALUES = 'parser-configuration-values';
-
-    /**
-     * @var string
-     */
-    private $documentUri = null;
-
-    /**
-     * Character set of document being validated.
-     * Only relevant if documentUri is of type file:/
-     *
-     * @see http://en.wikipedia.org/wiki/Character_encodings_in_HTML
-     * @var string
-     */
-    private $documentCharacterSet = null;
-
     private $commandFactory;
     private $commandExecutor;
 
@@ -34,33 +15,10 @@ class Wrapper
         $this->commandExecutor = $commandExecutor;
     }
 
-    public function configure(array $configurationValues = [])
+    public function validate(string $uri, string $documentCharacterSet): Output
     {
-        if (!isset($configurationValues[self::CONFIG_KEY_DOCUMENT_URI])) {
-            throw new \InvalidArgumentException(
-                sprintf('Configuration value "%s" not set', self::CONFIG_KEY_DOCUMENT_URI),
-                1
-            );
-        }
-
-        if (isset($configurationValues[self::CONFIG_KEY_DOCUMENT_CHARACTER_SET])) {
-            $this->documentCharacterSet = $configurationValues[self::CONFIG_KEY_DOCUMENT_CHARACTER_SET];
-        }
-
-        $this->documentUri = $configurationValues[self::CONFIG_KEY_DOCUMENT_URI];
-    }
-
-    public function validate(): Output
-    {
-        if (empty($this->documentUri)) {
-            throw new \InvalidArgumentException(
-                sprintf('Configuration value "%s" not set', self::CONFIG_KEY_DOCUMENT_URI),
-                2
-            );
-        }
-
         return $this->commandExecutor->execute(
-            $this->commandFactory->create($this->documentUri, $this->documentCharacterSet)
+            $this->commandFactory->create($uri, $documentCharacterSet)
         );
     }
 }
