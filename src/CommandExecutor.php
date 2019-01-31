7cefa1f3e82557660c23945a61d9af99c4fb9c85
Jon Cram <webignition@gmail.com>
2019-01-31 11:45:56 +0000

    Pass output parser flags to Wrapper::validate() (#20)

--- src/CommandExecutor.php
+++ src/CommandExecutor.php
@@ -2,6 +2,7 @@
 
 namespace webignition\HtmlValidator\Wrapper;
 
+use webignition\HtmlValidatorOutput\Parser\Flags;
 use webignition\HtmlValidatorOutput\Parser\Parser as OutputParser;
 use webignition\HtmlValidatorOutput\Models\Output;
 
@@ -14,10 +15,10 @@ class CommandExecutor
         $this->outputParser = $outputParser;
     }
 
-    public function execute(string $command): Output
+    public function execute(string $command, ?int $flags = Flags::NONE): Output
     {
         $validatorOutput = shell_exec($command);
 
-        return $this->outputParser->parse($validatorOutput);
+        return $this->outputParser->parse($validatorOutput, $flags);
     }
 }
